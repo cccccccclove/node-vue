2fd43fae68ce6e32c1a19b9d981288ebe3a8ff2f
chenkaiting <chenkaiting@pconline.com.cn>
2022-06-21 23:58:58 +0800

    start

---
+++ db/pool.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql')
+
+let pool = mysql.createPool({
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'root',
+    database: 'book'
+})
+
+module.exports = {
+    pool
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import select from './select.vue'
+const axios = require('axios')
+export default {
+  components: { select },
+  data() {
+    return {
+
+    }
+  },
+  created() {
+    let users
+    axios.post('http://localhost:8080/user/saveOrUpdate',{
+      username:"小碗",
+      password:'123',
+      gender:'女',
+      id:1
+    }).then((res) => {
+      users = res.data
+    })
+    console.log(users)
+  },
+}
+</script>
